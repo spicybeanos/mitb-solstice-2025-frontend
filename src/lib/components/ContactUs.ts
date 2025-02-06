84052ba4ec6571894b55e3da1275483e1da03176
Aryan D <aryan.d.dalal@gmail.com>
2025-02-06 23:07:37 +0530

    added contact us and stay page

---
+++ src/lib/components/ContactUs.ts
@@ -0,0 +1,12 @@
+export const information = [
+    {
+        position:"Fest president",
+        name:"Krishna Saraf",
+        contact:"Contact number / email"
+    },
+    {
+        position:"Fest <something>",
+        name:"Full Name",
+        contact:"Contact number / email"
+    }
+]
---
+++ src/routes/contactus/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { information } from "$lib/components/ContactUs";
+    import Card from "$lib/components/Card.svelte";
+</script>
+
+<div class="mid">
+    <div class="centre">
+        {#each information as info}
+        <div class="m-[5px]">
+            <Card title={info.position}>
+                <div class=" w-[280px]">{info.name}</div>
+                <div>{info.contact}</div>
+            </Card>
+        </div>
+            
+        {/each}
+    </div>
+</div>
+
+<style>
+    p{
+        margin: 5px;
+    }
+    .mid{
+        height: 60vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .centre {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        flex-direction: column;
+    }
+</style>
