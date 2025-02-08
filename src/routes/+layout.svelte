09a57692938a0636cd17c96d26f6f4e731f90673
Aryan D <aryan.d.dalal@gmail.com>
2025-02-08 19:55:17 +0530

    added icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
     import "./app.css";
+    import { UserProfileData } from "./GoogleLogin.svelte.ts";
+    import logo from "$lib/falak.png";
+    import profile_circle from "$lib/icons/account_circle.png"
+    import tickets_icon from "$lib/icons/ticket.png";
+    import support_icon from "$lib/icons/support.png";
+    import events_icon from "$lib/icons/event.png";
+    import stay_icon from "$lib/icons/stay.png";
+
     import ALink from "$lib/components/ALink.svelte";
     import { onMount } from "svelte";
     import { checkLoggedIn } from "./GoogleLogin.svelte.ts";
@@ -19,24 +27,41 @@
 </script>
 
 <nav>
-    <ALink style="margin:5px" href="/">Home</ALink>
-    <ALink style="margin:5px" href="/profile">Profile</ALink>
-    <ALink style="margin:5px" href="/events">Events & Conclaves</ALink>
-    <ALink style="margin:5px" href="/support">Support</ALink>
-    <ALink style='margin:5px' href='/stay'>Accommodation</ALink>
+    <ALink style="margin:5px" href="/"><img class="logo" src={logo} alt="logo" /></ALink>
+    <div>
+        <ALink style="margin:5px" href="/events"><img class="icon" src={events_icon} alt="events and conclaves"></ALink>
+        <ALink style="margin:5px" href="/support"><img class="icon" src={support_icon} alt="support"></ALink>
+        <ALink style="margin:5px" href="/passes"><img class="icon" src={tickets_icon} alt='passes'></ALink>
+        <ALink style="margin:5px" href="/stay"><img class="icon" src={stay_icon} alt='stay and accomodation'></ALink>
+    </div>
+
+    <ALink style="margin:5px" href="/profile">
+        {#if !UserProfileData.loggedIn}
+        <img class="icon" src={profile_circle} alt='profile'>
+        {:else}
+        <img class="icon" src={UserProfileData.picture} alt='profile'>
+        {/if}
+    </ALink>
 </nav>
 {@render children()}
 
 <style>
+    .logo{
+        width: max(7vw,10em);
+    }
+    .icon{
+        width: min(7vw,2em);
+    }
+    div{
+        display: flex;
+        justify-content: center;
+    }
     nav {
         padding: 15px;
         margin: 12px;
-        border-radius: 1em;
-        border-style: solid;
         flex-wrap: wrap;
         display: flex;
+        justify-content: space-between;
         flex-direction: row;
-        border-color: var(--border-light-mode);
-        box-shadow: 0px 6px black;
     }
 </style>
