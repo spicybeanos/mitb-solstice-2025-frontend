0afa17d4aefb4b8dfe4280f209e9c7c776834a4b
ashmit-p <ashmitp1012@gmail.com>
2025-02-10 16:49:57 +0530

    Update CardBody.svelte

--- src/lib/components/ui/ThreeDCardEffect/CardBody.svelte
+++ src/lib/components/ui/ThreeDCardEffect/CardBody.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-	import { cn } from '$lib/utils/cn.ts';
-
 	export let className: string;
 </script>
 
 <div
-	class={cn(
-		'h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]',
-		className
-	)}
+	class={
+		'h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]'}
 >
 	<slot />
 </div>
