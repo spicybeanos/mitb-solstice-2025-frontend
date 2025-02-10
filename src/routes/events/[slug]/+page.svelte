9d107ab0547e51fb9a871bf9e76734668fd441c4
ashmit-p <ashmitp1012@gmail.com>
2025-02-10 16:05:30 +0530

    Update [slug] +page.svelte

--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -3,7 +3,9 @@
     import { onMount } from "svelte";
     import {events} from "$lib/components/Events.js";
     import Card from "$lib/components/Card.svelte";
+    import { CardBody, CardContainer, CardItem } from "$lib/components/ui/ThreeDCardEffect/index.js";
 
+    let isMouseEntered = false;
     let { data } = $props();
     let event: FalakEvent = $state({
         img_alt:"picture of the event",
@@ -20,37 +22,63 @@
 
     onMount(() => {
         const eventID = data.slug;
+        
         events.forEach(element => {
             if(element.id === eventID){
                 event = element;
             }
         });
-    })
+    });
+    
 </script>
 <div class="centre main">
-<Card title={`@${event.id}`}>
-    <div class="mid">
-        <div style="font-size: xx-large;font-weight: 900;">{event.title}</div>
-        <img src={event.pictureURL} alt={event.img_alt}/>
-        <div style="text-align: center;">{event.description}</div>
-        <div> See you at {event.date} at {event.time} hrs!</div>
-        <div>The event is {event.duration} {event.duration === 1? 'hour':'hours'}</div>
-        <div>{#if event.teamSize <= 1}
-            This event is a solo event
-            {:else}
-            The max team size is {event.teamSize}
-            {/if}
-        </div>
-    </div>
-</Card>
+    <CardContainer bind:isMouseEntered className="inter-var ">
+        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto   h-auto rounded-xl p-6 border">
+            <CardItem {isMouseEntered} translateZ="100" className="w-full mb-4">
+                <img
+                    src={event.pictureURL}
+                    height="1000"
+                    width="1000"
+                    class="h-60 w-full rounded-xl object-fill group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                />
+            </CardItem>
+            <CardItem
+            {isMouseEntered}
+            translateZ="50"
+            className="text-xl w-full  text-[#C7AE93] flex mt-8 justify-between"
+            >
+                <div class="text font-extrabold">
+                    {event.name}   
+                </div>
+                <div class="text font-medium">
+                    {`@${event.id}`}
+                </div>
+            </CardItem>
+            <CardItem
+                {isMouseEntered}
+                translateZ="60"
+                className="text-[#C7AE93] text-sm max-w-sm mt-2 "
+            >
+                {event.description}
+            </CardItem>
+            <CardItem
+                {isMouseEntered}
+                translateZ="60"
+                className="text-[#C7AE93] text-sm max-w-sm mt-2 mb-4"
+            >
+                <pre>Team size: {` ${event.teamSize} ${event.teamSize === 1 ? ' person' : ' people'}`}</pre>
+            </CardItem>
+        </CardBody>
+    </CardContainer>
 </div>
 <style>
     img{
         width: 60vw;
     }
-    div{
+    /* div{
         color: var(--text-light)
-    }
+    } */
     .margin {
         margin: 10px;
     }
