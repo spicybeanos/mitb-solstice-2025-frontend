58770d8aad99671550a4329b72b20bfd5942340c
Aryan D <aryan.d.dalal@gmail.com>
2025-03-10 23:21:34 +0530

    changed color + event pass relation editor

--- src/routes/manage/eventpass/+page.server.ts
+++ src/routes/manage/eventpass/+page.server.ts
@@ -1,5 +1,5 @@
 import { verifyAndGetUser } from "$lib/server/Backend";
-import { check_EventRW_Access, check_manage_Access } from "$lib/server/BackendAdmin";
+import { check_EventRW_Access, check_manage_Access, check_PassRW_Access } from "$lib/server/BackendAdmin";
 import { getEvents } from "$lib/server/BackendAgentEvent.js";
 import { generateChecksum } from "$lib/server/CacheMaster";
 import { redirect } from "@sveltejs/kit";
@@ -8,7 +8,7 @@ export async function load({ cookies }) {
     const jwt = cookies.get('authToken');
     const userJson = cookies.get('userInfo');
     const checksum = cookies.get('userChecksum');
-    const canAccess = await check_manage_Access(jwt, userJson, checksum);
+    const canAccess = await check_PassRW_Access(jwt, userJson, checksum);
     if (userJson == null || checksum == null) {
         const user = await verifyAndGetUser(cookies.get('authToken'), userJson, checksum);
         if (user.result != null) {
