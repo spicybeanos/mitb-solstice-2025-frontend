fcb26546be3a2f40b3ed2b49258550a7003ad764
Aryan D <aryan.d.dalal@gmail.com>
2025-03-10 15:52:49 +0530

    given krishna admin perms

--- src/lib/server/BackendAdmin.ts
+++ src/lib/server/BackendAdmin.ts
@@ -2,7 +2,7 @@ import { verifyAndGetUser, type Result } from "./Backend";
 import { getEventInfo } from "./BackendAgentEvent";
 import { getUserId } from "./BackendAgentUser";
 
-const admins: string[] = ['aryan.d.dalal@gmail.com'];
+const admins: string[] = ['aryan.d.dalal@gmail.com','krishnasaraf1321@gmail.com'];
 const IT_OCs: string[] = ['blucraft2104@gmail.com'];
 const OPS_CC_team: string[] = ['sarveshbagla07@gmail.com', 'tyagi.aryan3434@gmail.com','aarushie1204@gmail.com','annanykapoor1304@gmail.com','shashwatshukla120@gmail.com','bagadiaakshat11@gmail.com','anushkamishra0102@gmail.com','sambhav02082004@gmail.com','harshitashahi26@gmail.com'];
 const HR_team: string[] = ['atharvamaik200@gmail.com', 'rohitnandagopal010305@gmail.com', 'suryanshabhijit1@gmail.com', 'aishanisharma3@gmail.com', 'snishtha.381@gmail.com', 'ayushi.khubber@gmail.com', 'kshitij.betwal@gmail.com'];
