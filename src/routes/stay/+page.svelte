16f2041df6b9882fcb593162102fc38774bdba49
Aryan D <aryan.d.dalal@gmail.com>
2025-04-02 10:52:40 +0530

    added atharva's info

--- src/routes/stay/+page.svelte
+++ src/routes/stay/+page.svelte
@@ -1,16 +1,19 @@
-<script lang='ts'>
-
+<script lang="ts">
+    import Card from "$lib/components/Card.svelte";
 </script>
 
 <div class="mid">
-    <div class="centre">
-        No information yet, but stay tuned!
+    <div class="m-[5px]">
+        <Card title=''>
+            <div class=" w-[280px]">Atharv Maikhuri</div>
+            <div class="text-white">+91 79069 52055</div>
+            <code class="text-white">atharvamaik200@gmail.com</code>
+        </Card>
     </div>
 </div>
 
 <style>
-   
-    .mid{
+    .mid {
         height: 60vh;
         display: flex;
         align-items: center;
